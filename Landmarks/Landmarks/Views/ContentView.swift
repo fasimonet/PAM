c49e57e1136dda33a87ae806949d7d7ac8423055
Fabien SIMONET <fasimonet@a106imac07.local>
2021-01-04 11:40:41 +0100

    :sparkles: Tuto 'Building Lists and Navigation'

---
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Fabien SIMONET on 04/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
