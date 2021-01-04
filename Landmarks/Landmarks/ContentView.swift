255bcc0483d21bf4c0e75c1d4c42a5cfce31a19c
Fabien SIMONET <fasimonet@a106imac07.local>
2021-01-04 09:05:21 +0100

    :sparkles: init TP2

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Fabien SIMONET on 04/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Bonjour le monde !")
+            .font(.title)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
